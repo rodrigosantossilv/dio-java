866f388bc85d7f063684a43d7093c53cc476921d
Rodri.ys <141459048+rodrigosantossilv@users.noreply.github.com>
2024-08-01 13:43:40 -0300

    Add files via upload

---
+++ java-anatomia/src/AnatomiaDois.java
@@ -0,0 +1,11 @@
+public class AnatomiaDois {
+    
+    public static void main (String [] args) {
+
+    final String BR = "Brasil";
+    final double PI = 3.14;
+    final int ESTADOS_BRASILEIRO = 27;
+    final int ano_200 = 2000;
+        
+    }
+}
---
+++ java-anatomia/src/AnatomiaUm.java
@@ -0,0 +1,9 @@
+public class AnatomiaUm {
+   
+public static void main (String [] args){
+
+    System.out.println("Ola,Mundo");
+}
+
+
+}
